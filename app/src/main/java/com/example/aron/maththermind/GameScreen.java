f8e6bfd237145cfcb427d87413e693634bceb731
Aron Gaden <aron.gaden@gmail.com>
2018-01-07 15:46:45 +0100

    added the necessary timers and made them functional, linked buttons etc.

--- app/src/main/java/com/example/aron/maththermind/GameScreen.java
+++ app/src/main/java/com/example/aron/maththermind/GameScreen.java
@@ -1,13 +1,24 @@
 package com.example.aron.maththermind;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by breinhold on 20.12.17.
  */
 
 public class GameScreen extends AppCompatActivity {
+    TextView tvCurrentTime,tvCurrentScore,tvExercise;
+    ImageView ivLife1,ivLife2,ivLife3;
+    EditText etInput;
+    int minute,second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +27,58 @@ public class GameScreen extends AppCompatActivity {
         initialize_activity();
     }
     public void initialize_activity(){
+        tvCurrentTime =(findViewById(R.id.txtView_currenttime));
+        tvCurrentScore=(findViewById((R.id.txtView_currentscore)));
+        tvExercise=(findViewById(R.id.txtView_exercise));
+        ivLife1=findViewById(R.id.imageView_life1);
+        ivLife2=findViewById(R.id.imageView_life2);
+        ivLife3=findViewById(R.id.imageView_life3);
+        etInput=findViewById(R.id.editTextInput);
+        minute=1;
+        second=60;
         timer_start();
     }
 
     public void timer_start(){
-        Timer timer = new Timer();
+        Timer gameTimer = new Timer(true);
+        gameTimer.schedule(new endGameTask(),1000*60);
+        gameTimer.schedule(new currentTimeTask(),1000,1000);
     }
-}
+
+    private class currentTimeTask extends TimerTask{
+
+        public void run(){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(second==60){
+                        tvCurrentTime.setText(minute+":00");
+                        minute--;
+                    }
+                    else if(second>9) {
+                        tvCurrentTime.setText(minute + ":" + second);
+                    }
+                    else{
+                        tvCurrentTime.setText(minute+":0"+second);
+                    }
+
+                    if(second>0){
+                        second--;
+                    }
+                }
+            });
+
+
+        }
+    }
+    private class endGameTask extends TimerTask{
+        public void run(){
+            Intent intent= new Intent(GameScreen.this, VictoryScreen.class);
+            startActivity(intent);
+        }
+
+    }
+    }
+
+
 
