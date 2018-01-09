8cb2660b07f608bfc4624c4bc1aad166e83441b4
Aron Gaden <aron.gaden@gmail.com>
2018-01-09 12:34:46 +0100

    Started on shared prefs

--- app/src/main/java/com/example/aron/maththermind/MainScreen.java
+++ app/src/main/java/com/example/aron/maththermind/MainScreen.java
@@ -1,10 +1,13 @@
 package com.example.aron.maththermind;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -41,21 +44,50 @@ public class MainScreen extends AppCompatActivity {
                 startOptionsActivity();
             }
         });
+
+        SharedPreferences spSound = this.getSharedPreferences("sound",MODE_PRIVATE);
+        SharedPreferences spLevel = this.getSharedPreferences("level", MODE_PRIVATE);
+        if(spSound.getInt("sfx",-3)==-3){
+            SharedPreferences.Editor soundEditor = spSound.edit();
+            soundEditor.putInt("sfx", 1);
+            soundEditor.putInt("music",1);
+            soundEditor.apply();
+            //DEBUGGING ensues
+            Toast.makeText(
+                    this, "Edited preferences: sfx, music on!", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        if(spLevel.getInt("lvlAdd",-3)==-3){
+            SharedPreferences.Editor lvlEditor = spLevel.edit();
+            lvlEditor.putInt("lvlAdd",1);
+            lvlEditor.putInt("lvlSubt",1);
+            lvlEditor.putInt("lvlDiv",1);
+            lvlEditor.putInt("lvlMult",1);
+            lvlEditor.apply();
+            Toast.makeText(
+                    this,"Edited preferences: enabled everything",Toast.LENGTH_LONG)
+                    .show();
+
+        }
     }
 
     private void startGameScreenActivity()
     {
+
         startActivity(new Intent(this, GameScreen.class));
     }
 
     private void startScoreBoardActivity()
     {
-        startActivity(new Intent(this, ScoreBoard.class));
+        startActivity(
+                new Intent(this, ScoreBoard.class));
     }
 
     private void startOptionsActivity()
     {
-        startActivity(new Intent(this, Options.class));
+        startActivity(
+                new Intent(this, Options.class));
     }
 
 
