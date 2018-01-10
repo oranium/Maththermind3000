630212aa280b936691d755e35208a746c05b694c
Aron Gaden <aron.gaden@gmail.com>
2018-01-10 10:12:57 +0100

    added multiplicators for each task(up for change) and result checker. also minor changes and bugfixes :^)

--- app/src/main/java/com/example/aron/maththermind/GameScreen.java
+++ app/src/main/java/com/example/aron/maththermind/GameScreen.java
@@ -1,7 +1,9 @@
 package com.example.aron.maththermind;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -9,7 +11,7 @@ import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
+import java.util.Random;
 /**
  * Created by breinhold on 20.12.17.
  */
@@ -19,6 +21,16 @@ public class GameScreen extends AppCompatActivity {
     ImageView ivLife1,ivLife2,ivLife3;
     EditText etInput;
     int minute,second;
+    int op1,op2,res;
+    int score;
+    short curOp;
+    short lives;
+    int addPoints,subtPoints,multPoints,divPoints;
+    int addDif,subtDif,multDif,divDif;
+    SharedPreferences spLevel;
+    Intent intent;
+    ConstraintLayout constraintLayout;
+    Timer gameTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,7 @@ public class GameScreen extends AppCompatActivity {
         initialize_activity();
     }
     public void initialize_activity(){
+
         tvCurrentTime =(findViewById(R.id.txtView_currenttime));
         tvCurrentScore=(findViewById((R.id.txtView_currentscore)));
         tvExercise=(findViewById(R.id.txtView_exercise));
@@ -36,11 +49,25 @@ public class GameScreen extends AppCompatActivity {
         etInput=findViewById(R.id.editTextInput);
         minute=1;
         second=60;
+        lives=3;
+        score = 0;
+        spLevel = getSharedPreferences("level",MODE_PRIVATE);
+        constraintLayout = new ConstraintLayout(this);
+        addDif=spLevel.getInt("lvlAdd",1);
+        subtDif=spLevel.getInt("lvlSubt",1);
+        multDif=spLevel.getInt("lvlMult",1);
+        divDif=spLevel.getInt("lvlDiv",1);
+        addPoints = 10*addDif;
+        subtPoints = 20*subtDif;
+        multPoints = 40*multDif;
+        divPoints = 60*divDif;
+
         timer_start();
+
     }
 
     public void timer_start(){
-        Timer gameTimer = new Timer(true);
+        gameTimer = new Timer(true);
         gameTimer.schedule(new endGameTask(),1000*60);
         gameTimer.schedule(new currentTimeTask(),1000,1000);
     }
@@ -74,12 +101,44 @@ public class GameScreen extends AppCompatActivity {
     }
     private class endGameTask extends TimerTask{
         public void run(){
-            Intent intent= new Intent(GameScreen.this, VictoryScreen.class);
+            gameTimer.cancel();
+            intent = new Intent(GameScreen.this, VictoryScreen.class);
             startActivity(intent);
         }
 
     }
+
+    //checks the result and adds points or subtracts lives
+    public void check_res(int userRes,short curOp){
+        //add corresponding number of points to score if task is right
+        if(userRes==res){
+            switch(curOp){
+                case 0: score+=addPoints; break;
+                case 1: score+=subtPoints; break;
+                case 2: score+=multPoints; break;
+                case 3: score+=divPoints; break;
+
+            }
+        //TODO: play right sound
+
+        //subtract a life otherwise and end the game if lives 0
+        }else{
+            //TODO: play wrong sound
+            lives--;
+            switch(lives){
+                case 2: constraintLayout.removeView(ivLife3);
+                case 1: constraintLayout.removeView(ivLife2);
+                case 0: constraintLayout.removeView(ivLife1);
+            }
+
+        }
+        if(lives==0){
+            gameTimer.cancel();
+            //TODO: play sad sound
+            startActivity(intent);
+        }
     }
+}
 
 
 
